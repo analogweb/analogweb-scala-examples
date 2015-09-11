ed4848f8d5e90d4d2f65ca6a4ae59a3ea85ce36d
snowgooseyk <snowgoose.yk@gmail.com>
2015-09-11 16:14:33 +0900

    Code diet

--- src/main/scala/org/analogweb/hello/Runner.scala
+++ src/main/scala/org/analogweb/hello/Runner.scala
@@ -1,20 +1,17 @@
 package org.analogweb.hello
 
 import java.net.URI
-
-import org.analogweb.core.Servers
-
 import com.google.inject._
-import org.analogweb.core.DefaultApplicationProperties
-import org.analogweb.guice.GuiceApplicationContext
+import org.analogweb.core.Servers
+import org.analogweb.core.DefaultApplicationProperties._
+import org.analogweb.guice.GuiceApplicationContext._
 
 object Runner {
 
   def main(args: Array[String]) = {
     val injector = Guice.createInjector(new HelloGuiceModule())
-    val context = GuiceApplicationContext.context(injector)
-    val props = DefaultApplicationProperties.defaultProperties()
-    Servers.create(URI.create("http://localhost:8080"),props,context).run()
+    val port = sys.props.get("http.port").getOrElse("9999")
+    Servers.create(URI.create("http://localhost:"+port),defaultProperties(),context(injector)).run()
   }
 
 } 
